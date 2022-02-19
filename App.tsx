a0c2d6ae2e39b2217624b744e1b90ba702b202f6
purplevarun <purplevarun@gmail.com>
2022-02-20 00:01:31 +0530

    legacy code

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppContainer from './src/components/app-container'
+import Navigator from './src/'
+export default function App() {
+  return (
+    <AppContainer>
+      <Navigator />
+    </AppContainer>
+  )
+}
